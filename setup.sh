6cafc52576c47f09cd2eb894422dacf73b13313c
Thibault Ghesquiere <ghesquie@lsmspc19.epfl>
2024-03-19 16:59:36 +0100

    Added a script for collisions of 3 spheres with elastic or inelastic rebound, solved with an LCP

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
-python3 -m venv .venv               # To comment after first use
+python3 -m venv .venv                                       # To comment after first use
 source .venv/bin/activate
-pip install -r requirements.txt     # To comment after first use
+pip install -r requirements.txt                             # To comment after first use
+
+# For Lemke's algorithm
+git clone https://github.com/AndyLamperski/lemkelcp.git     # To comment after first use
+cd lemkelcp                                                 # To comment after first use
+python3 setup.py install                                    # To comment after first use
